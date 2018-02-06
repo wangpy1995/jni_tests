5d541ddeaf748af7794366a749871b8e9a3ea67e
wpy <592114387@qq.com>
2018-02-07 01:55:39 +0800

    Initial commit

---
+++ src/test_ArrayOp.c
@@ -0,0 +1,14 @@
+//
+// Created by wpy on 2018/2/7.
+//
+#include <test_ArrayOp.h>
+
+JNIEXPORT void JNICALL Java_test_ArrayOp_arrayCopy
+(JNIEnv *env, jclass cls, jobject src, jint pos, jobject dest, jint dest_pos, jint len){
+
+}
+
+JNIEXPORT void JNICALL Java_test_ArrayOp_stringCopy
+        (JNIEnv * env, jclass cls, jstring str, jbyteArray arr){
+
+}
